6a179c812d1b44a54ee07a3302308cf5eecbd908
Amandeep Singh <a.d.scr71234@gmail.com>
2024-01-29 11:51:40 +0530

    adding backend to remote repo of git

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const fetchRouter = require("./routes/fetchRoutes");
+const authRouter = require("./routes/authRoutes");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const authMiddleware = require("./middleware/authMiddleware");
+require("dotenv").config();
+
+mongoose.set("strictQuery", true);
+
+const db_uri =
+  "mongodb+srv://1234amandeep:ilovefootball%401234@spyer.dtt7kqo.mongodb.net/auth?retryWrites=true&w=majority";
+
+console.log(process.env.DB_URI);
+
+const app = express();
+
+const corsOptions = {
+  origin: true,
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+// middleware
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(fetchRouter);
+app.use(authRouter);
+
+mongoose
+  .connect(db_uri)
+  .then(() => {
+    app.listen("3000", () => {
+      console.log("After connecting to auth db, listening at port 4000...");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// basic routes
+app.get("/root", authMiddleware.checkUser);
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,50 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user");
+require("dotenv").config();
+
+module.exports.checkUser = async (req, res, next) => {
+  // res.send("hey");
+  const token = req.cookies.jwt;
+  console.log("token inside middleware: ", token);
+
+  if (token) {
+    // verifying jwt token
+    // jwt.verify(token, process.env.SECRET_KEY, async (err, decodedToken) => {
+    jwt.verify(token, "spyer.io", async (err, decodedToken) => {
+      if (err) {
+        // token exist but not valid so user is not logged in
+        res.status(200).json({ user: null, msg: "token is invalid" });
+        next();
+      } else {
+        // token exist and its valid so user is logged in
+        const user = await userModel.findById(decodedToken.id);
+        res.status(200).json({
+          user: { _id: user._id, email: user.email, favList: user.favList },
+        });
+        next();
+      }
+    });
+  } else {
+    // token does'nt exist so user is not logged in
+    res.status(200).json({ user: null, msg: "token does not exists" });
+    next();
+  }
+};
+
+// module.exports.requireAuth = async (req, res, next) => {
+//   const token = req.cookies.jwt;
+
+//   if (token) {
+//     // verifying jwt token is valid
+//     jwt.verify(token, "spyer.io", async (err, decodedToken) => {
+//       if (err) {
+//         res.status(200).json({ user: null });
+//       } else {
+//         // user verified
+//         const user = await userModel.findById(decodedToken.id);
+//         next()
+//       }
+//     });
+//   }
+//   res.status(200).json({ user: null });
+// };
