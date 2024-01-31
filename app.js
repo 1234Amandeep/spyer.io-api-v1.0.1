7db8180eb08dfb6d59e0e38a57c13daad60f450a
Amandeep singh <75825122+1234Amandeep@users.noreply.github.com>
2024-01-31 12:36:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,14 +19,14 @@ console.log(process.env.DB_URI);
 
 const app = express();
 
-const corsOptions = {
-  origin: "*",
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
+// const corsOptions = ;
 
 // middleware
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: "*",
+  // credentials: true,
+  // optionsSuccessStatus: 200,
+}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(fetchRouter);
