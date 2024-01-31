c55a90d14740cbef037f7e0a1b640c71c58c5b81
Amandeep Singh <a.d.scr71234@gmail.com>
2024-01-31 20:42:02 +0530

    update in app.js

--- app.js
+++ app.js
@@ -17,15 +17,26 @@ const db_uri =
 console.log(process.env.DB_URI);
 
 const app = express();
+var allowlist = ["https://spyer-io-v1-0-1.web.app", "http://example2.com"];
 
-const corsOptions = {
-  origin: true,
-  credentials: true,
-  optionsSuccessStatus: 200,
+// const corsOptions = {
+//   origin: true,
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
+
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
 };
 
 // middleware
-app.use(cors(corsOptions));
+app.use(cors(corsOptionsDelegate));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(fetchRouter);
